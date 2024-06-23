36083bbb73197f7d9ed3d2f69649ef396af9c752
Yiming Xu <xuyiming.open@outlook.com>
2024-06-23 22:55:17 +0800

    init: Initial commit

---
+++ app/src/androidTest/java/io/github/xymopen/unhide/app/drawer/options/menu/button/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package io.github.xymopen.unhide.app.drawer.options.menu.button
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("io.github.xymopen.unhidelauncher3menu", appContext.packageName)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Unhide App Drawer Options Menu Button"
+include(":app")
