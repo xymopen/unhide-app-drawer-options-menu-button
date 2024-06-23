4f3c635addfc5023b55d8b41d8d97ecb185b62a8
Xu Yiming <xuyiming.open@outlook.com>
2024-06-23 23:04:26 +0800

    revert: Remove compatibility libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,6 @@ android {
 }
 
 dependencies {
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.appcompat)
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
